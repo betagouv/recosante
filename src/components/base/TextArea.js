a4260817ff10ae13ff29230dfb70b9b4e7ec77ea
Florian <hi@florianpanchout.com>
2021-04-02 15:07:30 +0200

    Inscription Patients

--- src/components/base/TextArea.js
+++ src/components/base/TextArea.js
@@ -7,12 +7,18 @@ const Wrapper = styled.div`
 const Label = styled.label`
   display: block;
   margin-bottom: 0.5rem;
+  color: ${(props) => props.theme.colors[props.error ? 'error' : 'text']};
+`
+const Subtitle = styled.p`
+  margin-bottom: 1rem;
+  font-size: 0.875rem;
+  opacity: 0.7;
 `
 const Input = styled.textarea`
   width: 100%;
-  padding: 0.5rem 1rem;
+  padding: 0.5em 0.75em;
   background-color: ${(props) => props.theme.colors.input};
-  box-shadow: inset 0 -2px 0 0 ${(props) => props.theme.colors.main};
+  box-shadow: inset 0 -2px 0 0 ${(props) => props.theme.colors[props.error ? 'error' : 'main']};
   border: none;
   resize: vertical;
   border-radius: 0.25rem 0.25rem 0 0;
@@ -20,12 +26,18 @@ const Input = styled.textarea`
 export default function TextArea(props) {
   return (
     <Wrapper>
-      {props.label && <Label htmlFor={props.name}>{props.label}</Label>}
+      {props.label && (
+        <Label htmlFor={props.name} error={props.error}>
+          {props.label}
+        </Label>
+      )}
+      {props.subtitle && <Subtitle>{props.subtitle}</Subtitle>}
       <Input
-        rows='10'
+        rows='3'
         id={props.name}
         name={props.name}
         value={props.value}
+        error={props.error}
         onChange={(e) => {
           props.onChange({ value: e.currentTarget.value, name: props.name })
         }}
--- src/pages/inscription-patients.js
+++ src/pages/inscription-patients.js
@@ -6,34 +6,156 @@ import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Web from 'src/components/layout/Web'
 import Section from 'src/components/layout/Section'
 import TextInputWithLabel from 'src/components/base/TextInputWithLabel'
+import TextArea from 'src/components/base/TextArea'
+import Button from 'src/components/base/Button'
+import Alert from 'src/components/base/Alert'
 
-const Form = styled.form``
-
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`
+const Submit = styled(Button)`
+  align-self: flex-end;
+`
 export default function InscriptionPatients(props) {
-  const [user, setUser] = useState({
-    name: '',
-    email: '',
-  })
+  const [user, setUser] = useState('')
+  const [patients, setPatients] = useState('')
+
+  const [error, setError] = useState(false)
+  const [invalidEmail, setInvalidEmail] = useState(false)
+  const [success, setSuccess] = useState(false)
+  const [code, setCode] = useState(null)
+
+  const createHeaders = () => {
+    let headers = new Headers()
+    headers.append('Content-Type', 'application/json')
+    headers.append(
+      'api-key',
+      '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'
+    )
+    return headers
+  }
+  const createContact = ({ patient, user }) =>
+    fetch('https://api.sendinblue.com/v3/contacts', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: patient,
+        attributes: {
+          MEDECIN: user,
+        },
+        listIds: [629],
+      }),
+      headers: createHeaders(),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.id) {
+          return 'success'
+        }
+        if (res.code === 'duplicate_parameter') {
+          return updateContact({ patient, user })
+        }
+        return res.code
+      })
+
+  const updateContact = ({ patient, user }) =>
+    fetch(`https://api.sendinblue.com/v3/contacts/${patient}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        attributes: {
+          MEDECIN: user,
+        },
+        listIds: [629],
+      }),
+      headers: createHeaders(),
+    }).then((res) => 'success')
+
+  const sendEmail = ({ patient }) =>
+    fetch('https://api.sendinblue.com/v3/smtp/email', {
+      method: 'POST',
+      body: JSON.stringify({
+        to: [
+          {
+            email: patient,
+          },
+        ],
+        templateId: 487,
+      }),
+      headers: createHeaders(),
+    })
+
   return (
     <Web title={'Recommander Recosanté'}>
       <Section small>
         <MDXRenderer>{props.data.mdx.body}</MDXRenderer>
-        <Form>
-          <TextInputWithLabel
-            label='Nom'
-            name='nom'
-            value={user.name}
-            onChange={({ value, name }) =>
-              setUser((prevUser) => ({ ...prevUser, [name]: value }))
+        <Form
+          onSubmit={(e) => {
+            e.preventDefault()
+            setSuccess(false)
+            setError(false)
+            if (!user || !patients) {
+              setError(true)
+            } else {
+              const patientsArray = patients.replaceAll(', ', ',').split(',')
+              const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+              const tempInvalidEmail = patientsArray.find(
+                (patient) => !re.test(String(patient).toLowerCase())
+              )
+              setInvalidEmail(tempInvalidEmail)
+
+              if (!tempInvalidEmail) {
+                setCode(success)
+                patientsArray.map((patient) =>
+                  createContact({ patient, user }).then((res) => {
+                    if (res === 'success') {
+                      sendEmail({ patient }).then((res) =>
+                        setCode(res.ok ? 'success' : 'error')
+                      )
+                    }
+                  })
+                )
+              }
             }
+          }}
+        >
+          <TextInputWithLabel
+            label='Votre nom et fonction'
+            name='name'
+            value={user.name}
+            error={error && !user.name}
+            onChange={({ value }) => setUser(value)}
           />
+          <TextArea
+            label='Les emails des patient·e·s que vous souhaitez inscrire'
+            subtitle='Entrez un ou plusieurs email(s) séparés par des virgules (,).'
+            name='patients'
+            value={user.patients}
+            error={error && !user.patients}
+            onChange={({ value }) => setPatients(value)}
+          />
+          <Submit>Envoyer</Submit>
+          {error && <Alert error>Merci de remplir tous les champs</Alert>}
+          {invalidEmail && (
+            <Alert error>
+              Le format de l'email {invalidEmail} n'est pas valide
+            </Alert>
+          )}
+          {success && <Alert>Merci ! Nous avons bien reçu votre message</Alert>}
+          {code && (
+            <Alert error={code !== 'success'}>
+              {code === 'success'
+                ? `Les invitations ont été envoyées`
+                : `Une erreur est survenue. Une ou plusieurs invitations n'ont pas pu être envoyé.`}
+            </Alert>
+          )}
         </Form>
       </Section>
     </Web>
   )
 }
 export const pageQuery = graphql`
-  query inscription {
+  query inscriptionpatients {
     mdx(slug: { eq: "inscription-patients" }) {
       body
     }
