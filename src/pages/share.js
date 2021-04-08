8e88513852180f41ce14af1737fed8379611500e
Florian <hi@florianpanchout.com>
2021-04-08 10:48:39 +0200

    Fix Width Share

--- src/pages/share.js
+++ src/pages/share.js
@@ -1,8 +1,13 @@
 import React, { useEffect } from 'react'
-
+import styled from 'styled-components'
 import Web from 'src/components/layout/Web'
 import Section from 'src/components/layout/Section'
 
+const Wrapper = styled.div`
+  width: 5rem;
+  height: 5rem;
+  background-color: blue;
+`
 export default function Index() {
   useEffect(() => {
     setTimeout(window.FB.XFBML.parse(), 1000)
@@ -11,13 +16,13 @@ export default function Index() {
     <Web title={'Mentions Légales'}>
       <Section>
         Link =
-        <div
+        <Wrapper
           className='fb-send-to-messenger'
           messenger_app_id='537843153878579'
           page_id='100297338851041'
         >
           Test
-        </div>
+        </Wrapper>
       </Section>
     </Web>
   )
