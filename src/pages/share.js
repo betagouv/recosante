e47a56092a7e8eac60e0355c344ac2a97686364b
Florian <hi@florianpanchout.com>
2021-04-08 10:41:44 +0200

    Add Page Id

--- src/pages/share.js
+++ src/pages/share.js
@@ -3,17 +3,18 @@ import React, { useEffect } from 'react'
 import Web from 'src/components/layout/Web'
 import Section from 'src/components/layout/Section'
 
-export default function Index(props) {
+export default function Index() {
   useEffect(() => {
-    window.FB.XFBML.parse()
+    setTimeout(window.FB.XFBML.parse(), 1000)
   }, [])
   return (
     <Web title={'Mentions Légales'}>
       <Section>
+        Link =
         <div
           className='fb-send-to-messenger'
           messenger_app_id='537843153878579'
-          page_id='863084903754981'
+          page_id='100297338851041'
         >
           Test
         </div>
