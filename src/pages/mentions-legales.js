0da82a682fd56f8d5f8c342bbc35f3f9aed4fb73
Florian <hi@florianpanchout.com>
2021-04-08 10:22:36 +0200

    FB Fix

--- src/pages/mentions-legales.js
+++ src/pages/mentions-legales.js
@@ -18,16 +18,8 @@ export default function Index(props) {
           messenger_app_id='537843153878579'
           page_id='863084903754981'
         >
-          lol
+          Test
         </div>
-        <a
-          href='https://www.facebook.com/dialog/share?
-    app_id=537843153878579
-    &href=https://recosante.beta.gouv.fr
-    &redirect_uri=https://recosante.beta.gouv.fr'
-        >
-          TEST
-        </a>
         <MDXRenderer>{props.data.mdx.body}</MDXRenderer>
       </Section>
     </Web>
