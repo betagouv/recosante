6c69cef563a40edd6c047290082b3603dcbfdd44
annepoirot <63290799+annepoirot@users.noreply.github.com>
2021-05-17 16:20:14 +0200

    Modifie wording message d'erreur

--- src/components/home/landing/SubscribeForm.js
+++ src/components/home/landing/SubscribeForm.js
@@ -67,7 +67,7 @@ export default function SubscribeForm() {
         }
         if (!optin) {
           setError(
-            `Vous devez accepter de recevoir des courriers électroniques de la part de Recosanté pour vous inscrire`
+            `Vous devez accepter de recevoir des emails de la part de Recosanté pour vous abonner`
           )
           window._paq &&
             window._paq.push([
