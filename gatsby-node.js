757d69287958361c99a22861dd49e9e82631d8cd
Florian <hi@florianpanchout.com>
2021-08-07 11:29:16 +0200

    Remove Places

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,22 +1,27 @@
 const axios = require(`axios`)
 
 exports.createPages = ({ actions: { createPage } }) => {
-  return axios
-    .get('https://geo.api.gouv.fr/communes?fields=departement,codesPostaux')
-    .then((res) => res.data)
-    .then((res) =>
-      res.forEach((place) => {
-        place.departement &&
-          createPage({
-            path: `/place/${place.code}/${place.nom
-              .toLowerCase()
-              .replace(' ', '-')
-              .replace(`'`, '-')
-              .normalize('NFD')
-              .replace(/[\u0300-\u036f]/g, '')}/`,
-            component: require.resolve('./src/templates/place.js'),
-            context: { place },
-          })
-      })
-    )
+  return (
+    axios
+      // .get('https://geo.api.gouv.fr/communes?fields=departement,codesPostaux')
+      .get(
+        'https://geo.api.gouv.fr/communes/75056?fields=departement,codesPostaux'
+      )
+      .then((res) => res.data)
+      .then((res) =>
+        [res].forEach((place) => {
+          place.departement &&
+            createPage({
+              path: `/place/${place.code}/${place.nom
+                .toLowerCase()
+                .replace(' ', '-')
+                .replace(`'`, '-')
+                .normalize('NFD')
+                .replace(/[\u0300-\u036f]/g, '')}/`,
+              component: require.resolve('./src/templates/place.js'),
+              context: { place },
+            })
+        })
+      )
+  )
 }
