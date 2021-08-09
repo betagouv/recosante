19a7dcc1334666926705edc238f3727eb5d8b75f
Florian <hi@florianpanchout.com>
2021-08-09 15:28:25 +0200

    Acquisition

--- src/components/form/side/recommandationList/Acquisition.js
+++ src/components/form/side/recommandationList/Acquisition.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import { useLocation } from '@reach/router'
 
-import Question from 'src/components/form/questions/Question'
+import { useProfile, useProfileMutation } from 'src/utils/api'
+import Wrapper from 'src/components/form/questions/question/Wrapper'
+import Answers from 'src/components/form/questions/question/Answers'
+import Button from 'src/components/base/Button'
 
+const StyledButton = styled(Button)`
+  align-self: flex-end;
+`
 export default function Animals(props) {
   const name = 'connaissance_produit'
   const label = [`J’ai connu Recosanté `, `J’ai connu Recosanté `]
@@ -31,5 +39,37 @@ export default function Animals(props) {
       label: `Autrement`,
     },
   ]
-  return <Question name={name} label={label} options={options} />
+
+  const location = useLocation()
+  const { data } = useProfile(location)
+  const mutation = useProfileMutation(location)
+
+  const [answers, setAnswers] = useState([])
+  useEffect(() => {
+    setAnswers(
+      data && data[name] ? (name === 'enfants' ? [data[name]] : data[name]) : []
+    )
+  }, [data, name])
+
+  const isCurrent = true
+
+  return data ? (
+    <Wrapper
+      onSubmit={(e) => {
+        e.preventDefault()
+        mutation.mutate({ [name]: answers })
+      }}
+      visible={answers.length || isCurrent}
+      isCurrent={isCurrent}
+    >
+      <Wrapper.Label>
+        {answers[0] === 'aucun' && !isCurrent ? label[1] : label[0]}
+      </Wrapper.Label>
+
+      <Wrapper.Response visible={isCurrent}>
+        <Answers options={options} answers={answers} setAnswers={setAnswers} />
+        <StyledButton hollow={answers.length}>Valider</StyledButton>
+      </Wrapper.Response>
+    </Wrapper>
+  ) : null
 }
