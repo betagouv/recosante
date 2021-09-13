768c9533d34326a35f04457cd70870af71985e71
Florian <hi@florianpanchout.com>
2021-09-13 21:46:13 +0200

    Fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -42,8 +42,8 @@ exports.createPages = ({ graphql, actions: { createPage } }) => {
           createPage({
             path: `/place/${place.code}/${place.nom
               .toLowerCase()
-              .replaceAll(' ', '-')
-              .replaceAll(`'`, '-')
+              .replace(/\s/g, '-')
+              .replace(/'/g, '-')
               .normalize('NFD')
               .replace(/[\u0300-\u036f]/g, '')}/`,
             component: require.resolve('./src/templates/place.js'),
