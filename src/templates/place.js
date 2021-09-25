ee2b7e3715137e4f6e7d011e86cb48192121856f
Florian <hi@florianpanchout.com>
2021-09-25 15:37:22 +0200

    Remove Footer from ssr

--- src/templates/place.js
+++ src/templates/place.js
@@ -2,19 +2,11 @@ import React from 'react'
 
 import Web from 'src/components/layout/Web'
 import Dashboard from 'src/components/Dashboard'
-import Newsletter from '../components/Newsletter'
-import Widget from 'src/components/Widget'
-import Data from 'src/components/Data'
-import About from 'src/components/About'
 
 export default function Place(props) {
   return (
     <Web title={props.pageContext.place.nom}>
       <Dashboard place={props.pageContext.place} />
-      <Newsletter />
-      <Widget home />
-      <Data />
-      <About />
     </Web>
   )
 }
