dc6cdd91bc51759a4cae108437312cc92282e54c
Florian <hi@florianpanchout.com>
2021-10-01 21:09:07 +0200

    Add Place Footer

--- src/templates/place.js
+++ src/templates/place.js
@@ -2,11 +2,17 @@ import React from 'react'
 
 import Web from 'src/components/layout/Web'
 import Dashboard from 'src/components/Dashboard'
+import Newsletter from '../components/Newsletter'
+import Data from 'src/components/Data'
+import About from 'src/components/About'
 
 export default function Place(props) {
   return (
     <Web title={props.pageContext.place.nom}>
       <Dashboard place={props.pageContext.place} />
+      <Newsletter />
+      <Data />
+      <About />
     </Web>
   )
 }
