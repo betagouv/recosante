6583f91c69f65cf5136019c324650e71b58547a9
Florian <hi@florianpanchout.com>
2021-10-08 19:09:11 +0200

    Issue 93

--- src/components/wrappers/ShareWrapper.js
+++ src/components/wrappers/ShareWrapper.js
@@ -10,38 +10,37 @@ export default function ShareWrapper(props) {
       messages={{
         mail: {
           simulator: {
-            subject: `Coucou, tu aimes le radon ?`,
-            body: `Bonjour,
-`,
+            subject: `Tu veux connaître la qualité de l'environnement autour de toi ?`,
+            body: `C'est facile grâce à Recosanté ! Tu peux choisir les indicateurs qui t'intéressent (qualité de l'air, risque d'allergie aux pollens, qualité de l'eau de boisson, etc.) et tu les reçois à la fréquence que tu choisis : tous les jours ou uniquement quand il y a une alerte. Recosanté propose aussi des recommandations associées pour limiter les effets de l'environnement sur la santé.
+Et en plus, c'est complètement gratuit. Pour t'inscrire : `,
           },
           result: {
-            subject: `Coucou, tu aimes le radon ?`,
-            body: `Bonjour,
-`,
+            subject: `Tu veux connaître la qualité de l'environnement à ${props.place?.nom} ?`,
+            body: `En t'inscrivant à Recosanté, tu peux recevoir les indicateurs qui t'intéressent (qualité de l'air, risque d'allergie aux pollens, qualité de l'eau de boisson, etc.) pour connaître la qualité de l'environnement à ${props.place?.nom} tous les matins ou en cas d'alerte. C'est gratuit et l'inscription est très simple : `,
           },
         },
         facebook: {
           simulator: {
-            quote: 'Coucou, tu aimes le radon ?',
+            quote: `Tu veux savoir si la qualité de l'air est bonne autour de chez toi ou s'il y a beaucoup de pollens aujourd'hui ? Inscris toi à Recosanté et reçois, à la fréquence que tu veux, des informations sur la qualité de ton environnement et sur les gestes à adopter pour protéger ta santé`,
           },
           result: {
-            quote: `${props.result} - Coucou, tu aimes le radon ?`,
+            quote: `Tu veux savoir si la qualité de l'air est bonne à ${props.place?.nom} ou s'il y a beaucoup de pollens aujourd'hui ? Inscris toi à Recosanté et reçois, à la fréquence que tu veux, des informations sur la qualité de ton environnement et sur les gestes à adopter pour protéger ta santé`,
           },
         },
         twitter: {
           simulator: {
-            title: 'Coucou, tu aimes le radon ?',
+            title: `Tu veux savoir si la qualité de l'air est bonne autour de chez toi ou s'il y a beaucoup de pollens aujourd'hui ? Inscris toi à Recosanté et reçois, à la fréquence que tu veux, des informations sur la qualité de ton environnement et sur les gestes à adopter pour protéger ta santé`,
           },
           result: {
-            title: `${props.result} - Coucou, tu aimes le radon ?`,
+            title: `Tu veux savoir si la qualité de l'air est bonne à ${props.place?.nom} ou s'il y a beaucoup de pollens aujourd'hui ? Inscris toi à Recosanté et reçois, à la fréquence que tu veux, des informations sur la qualité de ton environnement et sur les gestes à adopter pour protéger ta santé`,
           },
         },
         whatsapp: {
           simulator: {
-            title: 'Coucou, tu aimes le radon ?',
+            title: `Tu veux savoir si la qualité de l'air est bonne autour de chez toi ou s'il y a beaucoup de pollens aujourd'hui ? Inscris toi à Recosanté et reçois, à la fréquence que tu veux, des informations sur la qualité de ton environnement et sur les gestes à adopter pour protéger ta santé`,
           },
           result: {
-            title: `${props.result} - Coucou, tu aimes le radon ?`,
+            title: `Tu veux savoir si la qualité de l'air est bonne à ${props.place?.nom} ou s'il y a beaucoup de pollens aujourd'hui ? Inscris toi à Recosanté et reçois, à la fréquence que tu veux, des informations sur la qualité de ton environnement et sur les gestes à adopter pour protéger ta santé`,
           },
         },
       }}
