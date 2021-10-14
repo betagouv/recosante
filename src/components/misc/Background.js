83cef4026c63eef402108b1b46c651ff1f3f2337
Florian <hi@florianpanchout.com>
2021-10-14 11:44:31 +0200

    Fix iframe background

--- src/components/misc/Background.js
+++ src/components/misc/Background.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import useIframe from 'hooks/useIframe'
+
 const Wrapper = styled.div`
   position: absolute;
-  top: 0;
+  top: ${(props) => (props.iframe ? '5.75rem' : 0)};
   bottom: 0;
   left: -1rem;
   right: -1rem;
   background: linear-gradient(90deg, #d1edff 0%, #f8fafd 50%, #d6eeff 100%);
+  border-radius: ${(props) => (props.iframe ? '2rem' : 0)};
 `
 export default function Background() {
-  return <Wrapper />
+  const iframe = useIframe()
+
+  return <Wrapper iframe={iframe} />
 }
