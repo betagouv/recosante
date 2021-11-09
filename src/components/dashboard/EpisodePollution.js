dc41eb31f1620631e229425f98ceb2f4cf6726e6
Florian <hi@florianpanchout.com>
2021-11-09 18:16:47 +0100

    Issue 88

--- src/components/dashboard/EpisodePollution.js
+++ src/components/dashboard/EpisodePollution.js
@@ -1,18 +1,36 @@
 import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
 
 import useIndicators from 'hooks/useIndicators'
 import Modal from 'components/base/Modal'
 
+const Title = styled.h3`
+  strong {
+    text-transform: lowercase;
+  }
+`
+const Recommandation = styled.div`
+  font-weight: 300;
+`
 export default function EpisodePollution(props) {
   const { data } = useIndicators(props.place.code)
 
   const [open, setOpen] = useState(false)
   useEffect(() => {
-    data && setOpen(false)
+    data?.episodes_pollution?.advice && setOpen(false)
   }, [data])
+
   return (
     <Modal open={open} setOpen={setOpen}>
-      EpisodePollution
+      <Title>
+        Un <strong>{data?.episodes_pollution?.indice?.label}</strong> est prévu
+        aujourd'hui à {data?.indice_atmo?.validity?.area}
+      </Title>
+      <Recommandation
+        dangerouslySetInnerHTML={{
+          __html: data?.episodes_pollution?.advice?.main,
+        }}
+      />
     </Modal>
   )
 }
