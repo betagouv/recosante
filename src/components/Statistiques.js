30c7763a320108fa571c4cbb93ccf8a894fcf0be
fcoufour <github@coufourier.com>
2022-05-11 09:21:43 +0200

    mise à jour de la page Statistiques

--- src/components/Statistiques.js
+++ src/components/Statistiques.js
@@ -1,37 +1,75 @@
 import React from 'react'
-
+import styled from 'styled-components'
+import Section from 'components/base/Section'
 import useStatistiques from 'src/hooks/useStatistiques'
-import AllUsers from 'src/components/statistiques/AllUsers'
-import CurrentMonth from 'src/components/statistiques/CurrentMonth'
-import Profile from 'src/components/statistiques/Profile'
-import Satisfaction from 'src/components/statistiques/Satisfaction'
-import MailOpening from 'src/components/statistiques/MailOpening'
+import Email from 'src/components/statistiques/Email'
+import Web from 'src/components/statistiques/Web'
+
+export const MainTitle = styled.h1`
+  color: ${(props) => props.theme.colors.main};
+  font-size: 2.75rem;
+  margin-bottom: 5rem;
+`
+export const Subsection = styled(Section)`
+  margin-bottom: 5rem;
+`
+export const StyledSection = styled(Section)`
+  display: flex;
+  ${(props) => props.theme.mq.medium} {
+    flex-direction: column;
+  }
+  margin-bottom: 0;
+`
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 25rem;
+`
+export const Text = styled.h3`
+  flex: 1;
+  text-align: center;
+  margin-top: 2rem;
+  strong {
+    font-size: 4rem;
+  }
+`
 
 export default function Statistiques() {
-  const { stats, openings } = useStatistiques()
-
-  return stats && stats ? (
+  const { web, email, openings } = useStatistiques()
+  return web && web && email && email ? (
     <>
-      <CurrentMonth
-        inscriptions_desinscriptions={stats.inscriptions_desinscriptions}
-        totalActifs={stats.total_actifs}
-      />
-      {stats.total_actifs && (
-        <AllUsers
-          allUsers={JSON.parse(stats.all_users)}
-          totalInscriptions={stats.total_inscriptions}
+      {web.total_visits && (
+        <Web
+          total_visits={web.total_visits}
+          monthly_visits={JSON.parse(web.monthly_visits)}
+          place_monthly_visits={JSON.parse(web.place_monthly_visits)}
+          integration_widget={web.integration_widget}
+          integration_website={web.integration_website}
+          channel_search={web.channel_search}
+          channel_direct={web.channel_direct}
+          channel_website={web.channel_website}
+          channel_social={web.channel_social}
+          channel_campaign={web.channel_campaign}
         />
       )}
-      <Profile
-        total_allergies={stats.total_allergies}
-        total_pathologie_respiratoire={stats.total_pathologie_respiratoire}
-        total={stats.total_actifs}
-      />
-      {stats.decouverte && (
-        <Satisfaction satisfaction={JSON.parse(stats.decouverte)} />
-      )}
-      {openings && openings && openings.openings && (
-        <MailOpening openings={JSON.parse(openings.openings)} />
+      {email.total_actifs && (
+        <Email
+          total_actifs={email.total_actifs}
+          active_users={JSON.parse(email.active_users)}
+          desinscriptions={JSON.parse(email.desinscriptions)}
+          temps_moyen_inscription={email.temps_moyen_inscription} 
+          indicateur_indice_atmo={email.indicateur_indice_atmo}
+          indicateur_raep={email.indicateur_raep}
+          indicateur_vigilance_meteo={email.indicateur_vigilance_meteo}
+          indicateur_indice_uv={email.indicateur_indice_uv}
+          frequence_quotidien={email.frequence_quotidien}
+          frequence_alerte={email.frequence_alerte}
+          media_mail={email.media_mail}
+          media_notifications_web={email.media_notifications_web}
+          newsletter_hebdo={email.newsletter_hebdo}
+          total_utile={email.total_utile}
+          utile_oui={email.utile_oui}
+          opening={openings && openings && openings.opening}
+        />
       )}
     </>
   ) : null
---
+++ src/components/statistiques/Web.js
@@ -0,0 +1,147 @@
+import React, { useContext } from 'react'
+import Section from 'components/base/Section'
+import { MainTitle, Subsection, StyledSection, Wrapper } from 'components/Statistiques'
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  PieChart,
+  Pie,
+  Cell,
+} from 'recharts'
+import StyleContext from 'utils/StyleContext'
+
+export default function Website(props) {
+  const { themes, theme } = useContext(StyleContext)
+  const today = new Date()
+  const visitsData = Object.keys(props.monthly_visits).map((key) => ({
+    date: key,
+    all: props.monthly_visits[key],
+    place: props.place_monthly_visits[key],
+  }))
+  const totalIntegration = props.integration_widget + props.integration_website
+  const otherChannels = props.channel_campaign + props.channel_website + props.channel_social
+  const totalChannel = props.channel_search + props.channel_direct + otherChannels
+  const integrationData = [{
+    name: "Widget intégré",
+    value: props.integration_widget / totalIntegration
+  }, {
+    name: "Site web",
+    value: props.integration_website / totalIntegration
+  }]
+  const channelData = [{
+    name: "Moteur de recherche",
+    value: props.channel_search / totalChannel
+  }, {
+    name: "Accès direct",
+    value: props.channel_direct / totalChannel
+  }, {
+    name: "Autres canaux",
+    value: otherChannels / totalChannel
+  }]
+  const formatAsDecimal = (num) => (
+    new Intl.NumberFormat('fr-FR', {
+      style: 'decimal'
+    }).format(num)
+  )
+  const formatAsPercent = (num) => (
+    new Intl.NumberFormat('fr-FR', {
+      style: 'percent'
+    }).format(num)
+  )
+  const colors = [themes[theme].colors.main, '#6a6af4', '#cacafb']
+  return (
+    <Section first>
+      <MainTitle>
+        Le tableau de bord des indicateurs
+      </MainTitle>
+      <Subsection>
+        <Section.Title center>
+          <strong>{formatAsDecimal(props.total_visits)}</strong> visites
+        </Section.Title>
+        <Section.Subtitle center>
+          (au {today.toLocaleDateString()})
+        </Section.Subtitle>
+        <Wrapper>
+          <ResponsiveContainer>
+            <LineChart data={visitsData}>
+              <XAxis
+                dataKey='date'
+                tick={{ fontSize: 12 }}
+                interval='preserveStartEnd'
+              />
+              <YAxis tickFormatter={(value) => formatAsDecimal(value)} />
+              <Tooltip formatter={(value) => formatAsDecimal(value)} />
+              <Legend />
+              <Line
+                name='toutes les visites'
+                dataKey='all'
+                stroke={colors[0]}
+                strokeWidth={3}
+              />
+              <Line
+                name='visites du tableau de bord'
+                dataKey='place'
+                stroke={colors[1]}
+                strokeWidth={3}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </Wrapper>
+      </Subsection>
+      <Subsection>
+        <Section.Title center>
+          D’où viennent nos visiteurs ?
+        </Section.Title>
+        <StyledSection>
+          <Wrapper>
+            <ResponsiveContainer>
+              <PieChart>
+                <Pie
+                  dataKey='value'
+                  data={integrationData}
+                  cx='50%'
+                  cy='50%'
+                  outerRadius={'66%'}
+                  label={(entry) => formatAsPercent(entry.value)}
+                  isAnimationActive={false}
+                >
+                  {integrationData.map((entry, index) => (
+                    <Cell key={entry.name} fill={colors[index]} />
+                  ))}
+                </Pie>
+                <Tooltip formatter={(value) => formatAsPercent(value)} />
+                <Legend layout='vertical' align='center' verticalAlign='bottom' />
+              </PieChart>
+            </ResponsiveContainer>
+          </Wrapper>
+          <Wrapper>
+            <ResponsiveContainer>
+              <PieChart>
+                <Pie
+                  dataKey='value'
+                  data={channelData}
+                  cx='50%'
+                  cy='50%'
+                  outerRadius={'66%'}
+                  label={(entry) => formatAsPercent(entry.value)}
+                  isAnimationActive={false}
+                >
+                  {channelData.map((entry, index) => (
+                    <Cell key={entry.name} fill={colors[index]} />
+                  ))}
+                </Pie>
+                <Tooltip formatter={(value) => formatAsPercent(value)} />
+                <Legend layout='vertical' align='center' verticalAlign='bottom' />
+              </PieChart>
+            </ResponsiveContainer>
+          </Wrapper>
+        </StyledSection>
+      </Subsection>
+    </Section>
+  )
+}
