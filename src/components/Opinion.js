22d9dcaf94c6fef8bd23b8aadf34f8b1faa5e69b
fcoufour <github@coufourier.com>
2022-11-09 09:48:14 +0100

    rajout de l'appel à l'API dans tous les cas

--- src/components/Opinion.js
+++ src/components/Opinion.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useLocation } from '@reach/router'
 
+import { useAvis } from 'utils/api'
 import Section from 'components/base/Section'
+
 const Text = styled.p`
   ${(props) => props.theme.mq.small} {
     font-size: 1rem;
   }
 `
 export default function Opinion() {
+  const location = useLocation()
+  useAvis(location)
+
   return (
     <Section small first>
       <Section.Title>
