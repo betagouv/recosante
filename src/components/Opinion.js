d425ff05c0b2c697beae657583f15729cebdaf49
fcoufour <github@coufourier.com>
2022-11-09 09:23:16 +0100

    suppression de la saisie libre lorsque infolettre non utile

--- src/components/Opinion.js
+++ src/components/Opinion.js
@@ -1,31 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styled from 'styled-components'
-import { useLocation } from '@reach/router'
 
-import { useAvis, useAvisMutation } from 'utils/api'
 import Section from 'components/base/Section'
-import TextArea from 'components/base/TextArea'
-import Button from 'components/base/Button'
-import Alert from 'components/base/Alert'
-
 const Text = styled.p`
   ${(props) => props.theme.mq.small} {
     font-size: 1rem;
   }
 `
-const Form = styled.form``
-const ButtonWrapper = styled.div`
-  display: flex;
-  justify-content: flex-end;
-`
 export default function Opinion() {
-  const location = useLocation()
-
-  const { data } = useAvis(location)
-  const mutation = useAvisMutation(location)
-
-  const [message, setMessage] = useState('')
-
   return (
     <Section small first>
       <Section.Title>
@@ -37,31 +19,6 @@ export default function Opinion() {
         Merci pour votre contribution ! C'est grâce à vos retours que nous
         construisons un service utile !
       </Text>
-      {data && !data.appliquee && (
-        <Form
-          onSubmit={(e) => {
-            e.preventDefault()
-            mutation.mutate({ avis: message })
-          }}
-        >
-          <TextArea
-            value={message}
-            onChange={({ value }) => setMessage(value)}
-            label='Aidez-nous à comprendre pourquoi vous n’allez pas appliquer cette recommandation en écrivant quelques mots'
-          />
-          <ButtonWrapper>
-            <Button>Envoyer</Button>
-          </ButtonWrapper>
-          {mutation.isSuccess ||
-            (mutation.isError && (
-              <Alert error={mutation.isError}>
-                {mutation.isSuccess
-                  ? 'Merci de nous avoir envoyé votre avis'
-                  : 'Une erreur est survenue'}
-              </Alert>
-            ))}
-        </Form>
-      )}
     </Section>
   )
 }
