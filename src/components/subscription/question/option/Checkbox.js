f693c0d8f3cc130d91717b5770cb5ddc6abcfb94
fcoufour <github@coufourier.com>
2022-12-21 12:57:43 +0100

    Nu Html: Element div not allowed as child of element button in this context

--- src/components/subscription/question/option/Checkbox.js
+++ src/components/subscription/question/option/Checkbox.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Wrapper = styled.div`
+const Wrapper = styled.span`
   position: relative;
   display: flex;
   align-items: center;
